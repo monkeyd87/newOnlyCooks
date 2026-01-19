4091413965feb3af5cb7ebc3aade938f1be07c97
Dennis Jackson <dennisdjack1987@gmail.com>
2026-01-18 20:14:44 -0800

    add date to footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import GithubIcon from '@material-ui/icons/GitHub';
 
 function Footer() {
+    const year = new Date().getFullYear()
     return (
         <div className='footer'>
             <div className='socialMedia'>
                 <GithubIcon />
             </div>
             <p> Made with 🔥 by Dennis, Huy, Jon, Ryan, & Zue. </p>
-            <p> &copy; 2022 onlycooks.com </p>
+            <p> &copy; {year} onlycooks.com </p>
         </div>
     )
 }
